fb1bb4fb8977fba2e6719c79a1aa50b3409fe342
root <root@AlejoThinkPad>
2023-10-04 18:43:24 +0200

    ejecuta repetidamente script

---
+++ bulk_masscan.py
@@ -0,0 +1,31 @@
+import csv
+import json
+import os
+import subprocess
+
+# Ruta completa al archivo CSV
+csv_file = "iplist/es_development.csv"
+
+# Verificar si el archivo existe
+if os.path.exists(csv_file):
+  # Lista para almacenar los datos del CSV
+  ipList = []
+
+  # Leer el archivo CSV y almacenar los datos en la lista
+  with open(csv_file, newline='') as csvfile:
+    lector_csv = csv.DictReader(csvfile)
+    for file in lector_csv:
+      ipList.append({
+            "ip_enter":file['ip_enter'],
+            "ip_out": file['ip_out']
+      })
+  #Ejecutamos masscan
+  for ip in ipList:
+    ip_inicio = ip["ip_enter"]
+    ip_fin = ip["ip_out"]
+    command = f"masscan -p445 {ip_inicio}-{ip_fin} --max-rate 3000 > outputTest"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print(result)
+      
+else:
+  print("El archivo CSV no existe en la ruta proporcionada.")
