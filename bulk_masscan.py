4151fb7f36142c881ab2fbb36753b24a57e81009
root <root@AlejoThinkPad>
2023-10-10 16:19:51 +0200

    guardado

--- bulk_masscan.py
+++ bulk_masscan.py
@@ -20,14 +20,24 @@ if os.path.exists(csv_file):
             "ip_out": file['ip_out']
       })
   #Ejecutamos masscan
-  i = 0
+  totalFiles = 0
   for ip in ipList:
     ip_inicio = ip["ip_enter"]
     ip_fin = ip["ip_out"]
-    command = f"masscan -p445 {ip_inicio}-{ip_fin} --max-rate 3000 > ./output/outputTest{i}"
+    command = f"masscan -p445 {ip_inicio}-{ip_fin} --max-rate 3000 > ./output/outputRange{totalFiles}"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    i+=1
-  print(i)
-      
+    totalFiles+=1
+  print(totalFiles)
+  #Juntamos Ficheros salida en oput
+  file = "./output/output"
+  with open(file, 'w') as base_file:
+      for i in range(0, totalFiles):
+        fileName = f"./output/outputRange{i}"
+        try:
+          with open(fileName, 'r') as temporal_file:
+            #print(temporal_file.read())
+            base_file.write(temporal_file.read())   
+        except FileNotFoundError:
+            print(f"El fichero {base_file} no existe.")  
 else:
   print("El archivo CSV no existe en la ruta proporcionada.")
